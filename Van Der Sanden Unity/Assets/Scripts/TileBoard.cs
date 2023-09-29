9bef4615207b5bd4132aa639fd2d6f20696cc923
keano0102 <keano.pittevils@student.arcadiascholen.be>
2023-09-29 22:53:47 +0200

    tiles toegevoegd en beetje scripting

---
+++ Van Der Sanden Unity/Assets/Scripts/TileBoard.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileBoard : MonoBehaviour
+{
+    public Tile tilePrefab;
+    private TileGrid grid;
+    private List<Tile> tiles;
+}
+   
